c196566fa50875ff3ff228229e7a728a3b8c80b1
Doug Catharine <dougcatharine@gmail.com>
2020-07-30 21:50:11 -0600

    adding medium2 questions

---
+++ lesson3/medium2/question6.rb
@@ -0,0 +1,9 @@
+# Question6.rb
+# Doug Catharine
+# 20200730
+
+# How could the unnecessary duplication in this method be removed?
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
