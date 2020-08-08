30aaae38d485b9c5b6bc7bc3686eaacfd6ee2771
Doug Catharine <dougcatharine@gmail.com>
2020-08-08 10:24:04 -0600

    adding practice problems

---
+++ lesson5/practice_problems/pp1.rb
@@ -0,0 +1,9 @@
+# pp1.rb
+# Doug Catharine
+# 20200807
+
+# How would you order this array of number strings by descending numeric value?
+
+arr = ['10', '11', '9', '7', '8']
+
+arr.map { | str | str.to_i }
